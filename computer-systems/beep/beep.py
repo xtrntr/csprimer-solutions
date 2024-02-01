3f256a3a6e13d38dbdb5990d5e345100364cf6b2
krchia <kangren.chia@gmail.com>
2024-02-01 17:12:31 +0900

    beep

--- computer-systems/beep/beep.py
+++ computer-systems/beep/beep.py
@@ -1,13 +1,17 @@
 import sys
 import tty
 
-tty.setcbreak(0)
 
-while True:
-    ch = sys.stdin.read(1)
-    if ch.isdigit():
-        ch = int(ch)
-        for _ in range(ch):
-            sys.stdout.buffer.write(b'\x07')
-    else:
-        print(f"{ch} is not a ch.")
+try:
+    tty.setcbreak(sys.stdin.fileno()) # disables line buffering; allows program to read characters immediately without waiting for the user to press Enter.
+    while True:
+        ch = sys.stdin.read(1)
+        if ch.isdigit():
+            ch = int(ch)
+            for _ in range(ch):
+                sys.stdout.buffer.write(b'\x07')
+        else:
+            print(f"{ch} is not a ch.")
+        sys.stdout.flush()
+except KeyboardInterrupt as e:
+    sys.exit(0)
