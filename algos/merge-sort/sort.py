f4459e15fb40c4b683bd8a469972bf122083d1a3
krchia <kangren.chia@gmail.com>
2024-02-05 15:35:26 +0900

    mergesort

--- algos/merge-sort/sort.py
+++ algos/merge-sort/sort.py
@@ -1,8 +1,27 @@
+def merge(arr1, arr2):
+    print(f"merging arr1: {arr1}, arr2: {arr2}")
+    p1 = p2 = 0
+    rtn = []
+    while p1 < len(arr1) and p2 < len(arr2):
+        if arr1[p1] > arr2[p2]:
+            rtn.append(arr2[p2])
+            p2 += 1
+        else:
+            rtn.append(arr1[p1])
+            p1 += 1
+    if p1 < len(arr1):
+        rtn += arr1[p1:]
+    else:
+        rtn += arr2[p2:]
+    return rtn
+
+
 def mergesort(arr):
-    pass
-
-
-
+    print(f"mergesort call on {arr}")
+    length = len(arr)
+    if length > 1:
+        return merge(mergesort(arr[:length//2]), mergesort(arr[length//2:]))
+    return arr
 
 
 for arr in [
@@ -11,3 +30,4 @@ for arr in [
         [5, 3, 2, 2, 1, 9, 1], # duplicates
 ]:
     assert mergesort(arr) == sorted(arr), f"expected {sorted(arr)}, got {mergesort(arr)}"
+print("ok")
